fee10af8d49d14d877691dceb4cf0520cdd5a4ce
Katherinezhweds1 <Niyalp09@gmail.com>
2026-03-06 22:42:40 -0800

    code for states (final code)

--- src/autons.cpp
+++ src/autons.cpp
@@ -10,19 +10,24 @@ using namespace vex;
 
 //4 push red left //
 void leftAuton() {
-drivePID(16, 0.2, 0, 0);
-turnPID(90, 0.6, 0, 0);
-//DoubleActingPiston.set(false);  this is how you use pnuematics
-//DoubleActingPiston2.set(false);  this is how you use pnuematics
+DoubleActingPiston3.set(true);
+drivePID(-27.5, 0.18, 0, 0);
+turnPID(90.5, 0.2, 0, 0);
+runIntakeWithoutTopMotor();
+SingleActingPiston.set(true);
+DoubleActingPiston2.set(true);
+drivePID(6, 0.14, 0, 0);
+turnPID(-2, 0.23, 0, 0);
+drivePID(-12.3, 0.14, 0, 0);
+wait(1.2, sec);
+stopIntake();
+drivePID(21.5, 0.18, 0, 0 );
+scoreLongGoal();
 } 
 
 
 void rightAuton() {
-//Notes for Niya
-//  - current power 0.55 in PID.cpp
-//  - drive Kp= 0.29
-//  - turnkp = 0.37
-//. - when tuning PID start with Kp and drive power in PID.cpp check internial sensor is port 1 in robot-config.cpp1
+
 DoubleActingPiston3.set(true);
 drivePID(27.5, 0.18, 0, 0);
 turnPID(-90.5, 0.2, 0, 0);
