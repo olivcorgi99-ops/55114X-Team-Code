ab213b22c4e8ddd631ad55eece71d2278ab7dfe1
Katherinezhweds1 <Niyalp09@gmail.com>
2026-03-07 18:45:06 -0800

    auton : score 1 block

--- src/autons.cpp
+++ src/autons.cpp
@@ -11,42 +11,49 @@ using namespace vex;
 //4 push red left //
 void leftAuton() {
 DoubleActingPiston3.set(true);
-drivePID(-27.5, 0.18, 0, 0);
-turnPID(90.5, 0.2, 0, 0);
-runIntakeWithoutTopMotor();
-SingleActingPiston.set(true);
-DoubleActingPiston2.set(true);
-drivePID(6, 0.14, 0, 0);
-turnPID(-2, 0.23, 0, 0);
-drivePID(-12.3, 0.14, 0, 0);
-wait(1.2, sec);
-stopIntake();
-drivePID(21.5, 0.18, 0, 0 );
+drivePID(27.5, 0.15, 0, 0);
+turnPID(85, 0.17, 0, 0);
+drivePID(-20, 0.3, 0, 0);
 scoreLongGoal();
+
 } 
 
 
 void rightAuton() {
 
 DoubleActingPiston3.set(true);
+drivePID(26.5, 0.15, 0, 0);
+turnPID(-85, 0.17, 0, 0);
+drivePID(-20, 0.3, 0, 0);
+scoreLongGoal();
+wait(3, sec);
+drivePID(6, 0.3, 0, 0);
+
+    /*DoubleActingPiston3.set(true);
 drivePID(27.5, 0.18, 0, 0);
 turnPID(-90.5, 0.2, 0, 0);
 runIntakeWithoutTopMotor();
-SingleActingPiston.set(true);
 DoubleActingPiston2.set(true);
 drivePID(-6, 0.14, 0, 0);
 turnPID(2, 0.23, 0, 0);
-drivePID(12.3, 0.14, 0, 0);
-wait(1.2, sec);
+drivePID(11, 0.14, 0, 0);
+drivePID(-2, 0.14, 0, 0);
+drivePID(1.5, 0.14, 0, 0);
+wait(0.8, sec);
 stopIntake();
-drivePID(-21.5, 0.18, 0, 0 );
-scoreLongGoal();
+drivePID(-10, 0.18, 0, 0 );
+turnPID(-3, 0.23, 0, 0);
+drivePID(-12.5, 0.14, 0, 0);
+scoreLongGoal(); */
 
 }
 
 //NO AUTONNNNNNNNN AHHHHHHHHHHHHHHHHHHH//
 void skillsAuton() {
-drivePID(10, 0.14, 0, 0);
+    drivePID(-10, 0.14, 0, 0);
+    DoubleActingPiston2.set(true);
+    drivePID(50, 0.8, 0, 0);
+
 
 }
   
@@ -54,4 +61,4 @@ void noauton() {
 drivePID(16, 0.3, 0, 0);
 }
 
-          
+       
